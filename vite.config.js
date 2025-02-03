93717a5a0ab85686beb765e8b4be8f6fee35595d
Fabio <fasfort@gmail.com>
2025-02-03 14:19:48 -0300

    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/v2/',
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
